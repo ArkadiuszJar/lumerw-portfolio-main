cc22b29950cfae181974d8eccf79e9cd957b810f
Arek Jarnecki <129734809+ArkadiuszJarnecki@users.noreply.github.com>
2023-05-14 22:23:00 +0200

    add hand

--- src/pages/home/components/Landing/Landing.tsx
+++ src/pages/home/components/Landing/Landing.tsx
@@ -11,7 +11,7 @@ export const Landing = () => {
 
       <div className={styles.content}>
         <h1 className={styles.title}>
-          HEY ARE YOU LOOKING
+          HEY 👋 ARE YOU LOOKING
           <br />
           FOR UX/UI DESIGNER? WELL…
           <br />I KNOW SOMEONE WHO’S
